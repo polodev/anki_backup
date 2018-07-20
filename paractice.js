eaa474b5a49ca70c0d0b7bea7902c5ff04b42821
polodev <polodev10@gmail.com>
2018-07-20 10:30:25 +0600

    added file and some content in jquery

---
+++ paractice.js
@@ -0,0 +1,9 @@
+/**
+ * stopAll parameter specifies whether also the 
+ *  animation queue should be cleared or not.
+ *  Default is false, which means that only
+ *  the active animation will be stopped,
+ *  allowing any queued animations to be
+ *  performed afterwards.
+ */
+$(selector).stop(stopAll,goToEnd);
